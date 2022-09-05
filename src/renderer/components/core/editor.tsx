1cf756f9461f7022d0a79631e50a4692e55cd36b
mauro-balades <mauro.balades@tutanota.com>
2022-09-05 07:53:50 +0200

    errors and warnings are now identified

--- src/renderer/components/core/editor.tsx
+++ src/renderer/components/core/editor.tsx
@@ -13,8 +13,7 @@ export default ({tab}: any) => {
     const editorRef = useRef(null);
     let theme = useTheme();
 
-    const [errorCount, setErrorCount] = useState(0);
-    const [warningsCount, setWarningsCount] = useState(0);
+    const [markersCount, setMarkersCount] = useState({ warns: 0, errors: 0});
     const [tabwidth, setTabWith] = useState(4 /* best tab width */);
     const [cursorPosition, setCursorPosition] = useState({
         x: 0,
@@ -33,7 +32,19 @@ export default ({tab}: any) => {
             const model = editor.getModel();
             const markers = monaco.editor.getModelMarkers(model);
 
-            console.log({ markers })
+            let warnings = 0;
+            let errors = 0;
+
+            for (let marker of markers) {
+                if (marker.severity == 8) {
+                    errors++;
+                    continue;
+                }
+
+                warnings++;
+            }
+
+            setMarkersCount({ warns: warnings, errors })
         })
 
         editor.onDidChangeCursorPosition((event: /*ICursorPositionChangedEvent*/ any) => {
@@ -57,11 +68,11 @@ export default ({tab}: any) => {
             <EditorBottomNav>
                 <BottomNavIcon>
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                    <span>{warningsCount}</span>
+                    <span>{markersCount.warns}</span>
                 </BottomNavIcon>
                 <BottomNavIcon>
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
-                    <span>{errorCount}</span>
+                    <span>{markersCount.errors}</span>
                 </BottomNavIcon>
                 <BottonNavInformation>
                     <BottomInfoBtn>
